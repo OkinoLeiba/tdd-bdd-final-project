69f632ae94431367334e314b99559af79f21f49b
okinoleiba <okino.leiba@yahoo.com>
2025-11-26 21:11:23 -0500

    complete code

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -38,7 +38,7 @@ def step_impl(context):
     """ Make a call to the base URL """
     context.driver.get(context.base_url)
     # Uncomment next line to take a screenshot of the web page
-    # context.driver.save_screenshot('home_page.png')
+    context.driver.save_screenshot('home_page.png')
 
 @then('I should see "{message}" in the title')
 def step_impl(context, message):
@@ -132,3 +132,35 @@ def step_impl(context, element_name, text_string):
     )
     element.clear()
     element.send_keys(text_string)
+
+
+@when('I press the "{button}" button')
+def step_impl(context, button):
+    button_id = button.lower() + '-btn'
+    context.driver.find_element_by_id(button_id).click()
+
+
+@then('I should see "{name}" in the results')
+def step_impl(context, name):
+    # Use the WebDriverWait to wait for the specified name to be present in the element with the ID 'search_results
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element_value((By.ID, 'search_results'), name)
+    )
+    # Check if the provided name is present in the text content of the element using the expected_conditions.text_to_be_present_in_element method
+    # Use the assert(found) statement to verify the name was found in the results.
+    assert(element)
+
+@then('I should not see "{name}" in the results')
+def step_impl(context, name):
+    element = context.driver.find_element_by_id('search_results')
+    assert(name not in element.text)
+
+@then('I should see the message "{message}"')
+def step_impl(context, message):
+    element = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID, 'flash_message'),
+            message
+        )
+    )
+    assert(element)
